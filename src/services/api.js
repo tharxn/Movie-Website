4e3e02adab640f2326bc784b40c85cce4aa4ff11
Tharun17 <146533827+tharxn@users.noreply.github.com>
2025-07-10 09:06:17 +0530

    Add files via upload

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "bf101b85eb869256faa2b7fb481031ed";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
